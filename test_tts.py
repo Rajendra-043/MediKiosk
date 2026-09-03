dc3730d21a022a3d2f6d0953d9508622d1446331
Rajendra-43 <rajendrasinghwakre017@gmail.com>
2026-09-04 03:44:13 +0530

    Remove unwanted files

--- test_tts.py
+++
@@ -1,3 +0,0 @@
-from voice.services import speak_text
-
-speak_text("Hello. I am MediKiosk. How can I help you?")
--- voice/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path ,include
-from .views import voice_chat
-
-
-urlpatterns = [
-    path("chat/", voice_chat, name="voice_chat"),
-
-    path("voice/", include("voice.urls")),
-]
